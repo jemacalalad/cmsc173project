30516e2337b41b24d03f829109cdbe9890f03752
jemcalalad <jemacalalad@up.edu.ph>
2021-06-08 04:00:50 +0800

    added sticky add button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './index.css';
 import { useState } from "react";
 import Card from "./Card"
+import plus from "./icons/plus.png";
 
 function App() {
+  // experimental feature, don't modify
+  const [page, setPage] = useState("home");
+
+  // experimental feature, don't modify
+  const handleClick = (pageValue) => {
+    setPage(pageValue);
+  };
+
   const [cards, setCards] = useState([
+    {subject: "CMSC137", tasks: []},
+    {subject: "CMSC173", tasks: []},
+    {subject: "CMSC180", tasks: []},
+    {subject: "CMSC190", tasks: []},
+    {subject: "CMSC142", tasks: []},
     {subject: "CMSC137", tasks: []},
     {subject: "CMSC173", tasks: []},
     {subject: "CMSC180", tasks: []},
@@ -13,7 +26,7 @@ function App() {
   ]);
 
   return (
-    <div className="px-5 bg-indigo-900 h-screen">
+    <div className="px-5 bg-indigo-900 h-full w-full">
       <div className="px-5 py-5">
         <span className="font-sans font-bold text-white text-7xl">Most Recent</span>
       </div>
@@ -23,9 +36,18 @@ function App() {
       {/* grid of cards */}
       <div className="grid gap-2 grid-cols-4 justify-around">
         {cards.map(card => (
-          <Card subject={card.subject} tasks={card.tasks}/>
+          <Card subject={card.subject} tasks={card.tasks} click={handleClick}/>
         ))}
       </div>
+      <div className="fixed bottom-16 right-16 rounded-full bg-white w-20 h-20">
+        <button className="w-20 h-20 text-5xl" onClick={() => {console.log("ADD")}}>
+          <img className="w-full h-full" src={plus} alt="add"/>
+        </button>
+      </div>
+      {/* Experimental feature, please don't modify */}
+      {/* <div className="text-white text-xl">
+        <span>{page}</span>
+      </div> */}
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
 
-function Card({subject}){
+function Card({subject, tasks, click}){
     const colorList = ["green", "blue", "red", "purple", "yellow", "pink"];
 
     const color = colorList[Math.floor(Math.random()*colorList.length)];
@@ -14,7 +14,7 @@ function Card({subject}){
         // the card
         <div className={`m-5 flex flex-col bg-gradient-to-r from-${color}-200 to-${color2}-200 rounded-xl w-72`}>
             <span className="py-5 place-self-center font-sans font-bold text-3xl">{subject}</span>
-            <button className={`my-5 p-3 font-bold text-white place-self-center w-max bg-${color}-600 hover:bg-${color}-400 active:bg-${color}-800 rounded-xl`} onClick={() => {console.log("Clicked")}}>View</button>
+            <button className={`my-5 p-3 font-bold text-white place-self-center w-max bg-${color}-600 hover:bg-${color}-400 active:bg-${color}-800 rounded-xl`} onClick={() => {click(subject)}}>View</button>
         </div>
     );
 }
