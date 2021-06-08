c8b049faf516df1021ad19c36c476e9eda1f8d41
johnmichaelt <jstugay@up.edu.ph>
2021-06-08 21:53:33 +0800

    (follow pre-code commands) Redesign: input focus, background, fonts, etc.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './index.css';
 import { useState } from "react";
 import Card from "./Card"
 import plus from "./icons/plus.png";
+import Pomo from './Pomo';
 
 function App() {
   // experimental feature, don't modify
@@ -44,21 +45,19 @@ function App() {
     {subject: "CMSC173", tasks: []},
     {subject: "CMSC180", tasks: []},
     {subject: "CMSC190", tasks: []},
-    {subject: "CMSC142", tasks: []},
+    { subject: "CMSC142", tasks: [] },
     {subject: "CMSC137", tasks: []},
     {subject: "CMSC173", tasks: []},
-    {subject: "CMSC180", tasks: []},
-    {subject: "CMSC190", tasks: []},
-    {subject: "CMSC142", tasks: []}
   ]);
 
   return (
-    <div className="px-5 bg-indigo-900 h-full w-full">
+    <div className="px-10 py-7 bg-gray-300 min-h-screen min-w-screen">
+    
       <div className="px-5 py-5">
-        <span className="font-sans font-bold text-white text-7xl">Most Recent</span>
+        <span className="font-sans font-family: Roboto font-bold text-gray-900 text-5xl">Most Recent</span>
       </div>
-      <div className="px-5 py-3">
-        <span className="font-sans font-semibold text-white text-2xl">Go back to what you were working with...</span>
+      <div className="px-5 -mt-4 mb-6">
+        <span className="font-sans font-family: Roboto font-light text-gray-900 text-lg">Go back to what you were working with...</span>
       </div>
       {/* grid of cards */}
       <div className="grid gap-2 grid-cols-4 justify-around">
@@ -66,7 +65,7 @@ function App() {
           <Card subject={card.subject} tasks={card.tasks} click={handleClick}/>
         ))}
       </div>
-      <div className="fixed bottom-16 right-16 rounded-full bg-white w-20 h-20">
+      <div className="fixed bottom-12 right-12 rounded-full bg-transparent w-20 h-20">
         <button className="w-20 h-20 text-5xl" onClick={() => {newCard()}}>
           <img className="w-full h-full" src={plus} alt="add"/>
         </button>
@@ -81,23 +80,23 @@ function App() {
       {isOpen ? (
         <>
           <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            className="blur justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-2xl">
+              <div className="border-0 rounded-xl shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold"> New Card </h3>
+                  <h3 className="text-3xl font-sans font-family: Roboto font-bold"> New Pack </h3>
                 </div>
                 <div className="relative p-6 flex-auto">
                 <form onSubmit = {handleSubmit}>
                     <label className="font-sans my-4 text-blueGray-500 text-lg leading-relaxed">
-                      Card Name: 
-                      <input type="text" id = "name" name= "name" placeholder =" Enter card name"/>
+                      Pack Name:
+                      <input autoFocus border="hidden" type="text" id = "name" name= "name" placeholder =" Enter pack name"/>
                     </label>
                     <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                       
                       <button
-                        className="font-sans text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                        className="font-sans font-family: Roboto text-red-500 background-transparent font-normal uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                         type="button"
                         onClick={() => setOpen(false)} > Cancel </button>
                       
@@ -113,8 +112,6 @@ function App() {
         </>
       ) : null}
     </div>
-    
-    
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -12,9 +12,9 @@ function Card({subject, tasks, click}){
 
     return(
         // the card
-        <div className={`m-5 flex flex-col bg-gradient-to-r from-${color}-200 to-${color2}-200 rounded-xl w-72`}>
-            <span className="py-5 place-self-center font-sans font-bold text-3xl">{subject}</span>
-            <button className={`my-5 p-3 font-bold text-white place-self-center w-max bg-${color}-600 hover:bg-${color}-400 active:bg-${color}-800 rounded-xl`} onClick={() => {click(subject)}}>View</button>
+        <div className={`m-5 flex flex-col bg-gradient-to-r from-${color}-300 to-${color2}-300 rounded-xl w-72`}>
+            <span className="py-5 place-self-center font-sans font-family: Roboto  font-bold text-3xl">{subject}</span>
+            <button className={`my-5 px-3 py-2 font-bold text-white place-self-center w-max bg-${color}-400 hover:bg-${color}-600 active:bg-${color}-800 rounded-xl`} onClick={() => {click(subject)}}>View</button>
         </div>
     );
 }
