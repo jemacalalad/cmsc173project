a379eb236796418bd4be1dcbc6718d0f406aff45
AlinaCV <acvaethbrueckner@up.edu.ph>
2021-06-11 14:04:00 +0800

    added add Task

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
 
   const [pomo, setPomo] = useState("none");
   const [pomoTasks, setPomoTasks] = useState([]);
+  const [taskOpen, setTaskOpen] = useState(false);
 
   const openPomo = (subject, tasks) => {
      setPomo(subject);
@@ -38,6 +39,10 @@ function App() {
     setOpen(!isOpen);
   }
 
+  const newTask = () => {
+    setTaskOpen(!taskOpen);
+  }
+
   //handle the submission of a new card name
   const handleSubmit = (event) =>{
     event.preventDefault();
@@ -52,7 +57,24 @@ function App() {
       tempCard.tasks= [];
       setCards(cards => [tempCard, ...cards ]);
       //close the popup form
-      setOpen(!isOpen);
+      setOpen(!taskOpen);
+    }
+    
+  }
+
+  const handleTaskSubmit = (event) =>{
+    event.preventDefault();
+    let task = event.target.elements.name?.value.trim();
+    //if no name was entered alert the user to enter a name before submitting
+    if(task === ""){
+      alert("Enter a Task");
+    }else{
+      //if name was submitted, create and object and add it to the useState for cards
+      let tempTask = {};
+      tempTask.name = task;
+      setPomoTasks(pomoTasks => [...pomoTasks, tempTask]);
+      //close the popup form
+      setTaskOpen(!taskOpen);
     }
     
   }
@@ -77,7 +99,48 @@ function App() {
               <span className="font-sans font-family: Roboto font-bold text-gray-900 text-7xl select-none">App name?</span>
             </div>
             {isPomo ? (
+              <>
             <Pomo subject = {pomo} task_list = {pomoTasks} backClick = {closePomo}/>
+            <div className="fixed bottom-12 right-12 rounded-full bg-transparent w-20 h-20 hover:bg-gray-500 transition-all">
+              <button className="w-20 h-20 text-5xl" onClick={() => {newTask()}}>
+                <img className="w-full h-full" src={plus} alt="add"/>
+              </button>
+            </div>
+            {taskOpen ? (
+              <>
+                <div
+                  className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none backdrop-filter backdrop-blur"
+                >
+                  <div className="relative w-auto my-6 mx-auto max-w-2xl">
+                    <div className="border-0 rounded-xl shadow-lg relative flex flex-col w-full bg-white outline-none">
+                      <div className="flex px-5 pt-5 rounded-t outline-none">
+                        <h3 className="text-3xl font-sans font-family: Roboto font-bold"> New Task </h3>
+                      </div>
+                      <div className="relative p-6 mt-3 flex-auto">
+                        <form onSubmit = {handleTaskSubmit}>
+                          <label className="select-none font-sans p-5 text-blueGray-500 text-lg leading-relaxed">
+                            Task:
+                            <input autoFocus className="ml-3 p-2 bg-black text-white focus:border-0" type="text" id = "name" name= "name" placeholder =" Enter task"/>
+                          </label>
+                          <div className="flex mt-8 justify-end rounded-b">
+                            
+                            <button
+                              className="select-none font-sans font-family: Roboto font-normal text-red-500 text-sm uppercase bg-transparent mr-3 px-6 py-3 rounded-xl ease-linear hover:bg-red-600 hover:font-light hover:text-white focus:outline-none focus:ring focus:border-red-600 transition-all"
+                              type="button"
+                              onClick={() => setTaskOpen(false)} > Cancel </button>
+                            
+                            <input className="select-none font-sans font-family: Roboto font-bold text-white text-sm uppercase bg-black px-6 py-3 rounded-xl ease-linear shadow outline-none hover:bg-gray-500 hover:shadow-xl focus:outline-none focus:ring focus:border-white transition-all"
+                            type="submit" value= "Add Task" />
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="opacity-0 fixed inset-0 z-40 bg-black"></div>
+              </>
+          ) : null}
+            </>
           ) : null}
             {!isPomo ? (
               <>
